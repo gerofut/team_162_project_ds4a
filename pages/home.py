08428186448e63e75bc298302b4fdd063fdc5277
gerofut <gerofut@hotmail.com>
2022-07-02 17:38:45 +0000

    version 2.0

--- pages/home.py
+++ pages/home.py
@@ -1,38 +1,146 @@
+from doctest import OutputChecker
+from turtle import color
 import dash
 import pandas as pd
-from dash import html , dcc
+from dash import html , dcc, callback, Input, Output
 import dash_bootstrap_components as dbc
 from dash_labs.plugins import register_page
 import plotly.express as px
 
 
+## Register the page in dash_labs_plugin
+
 register_page(__name__, path="/")
 
-df = pd.read_csv('/home/crnox95/ds4a_project/data/csv/line_plot.csv')
+## Define the dataframe and prepare the data for the plot
+
+## Figure 1: line-plot
+df = pd.read_csv('/home/crnox95/ds4a_project/data/csv/line_plot.csv',sep=',')
 df['year_month']=pd.to_datetime(df['year_month']).dt.to_period('M')
 df.rename(columns={'year_month.1':'count'}, inplace=True)
 df.set_index('year_month', inplace=True)
-fig = px.line(df, x=df.index.to_timestamp(), y="count", title='H & M Sales by Month')
 
+## Figure 2: pie-plot
+df1 = pd.read_csv('/home/crnox95/ds4a_project/data/csv/pie_plot_home.csv',sep=',')
+df1['year_month'] = pd.to_datetime(df1['year_month']).dt.to_period('M')
+df1.set_index('year_month', inplace=True)
+
+## Card2: amount of money
+df2 = pd.read_csv('/home/crnox95/ds4a_project/data/csv/card2.csv',sep=',')
+df2.set_index('year_month', inplace=True)
+
+## Define the layout of the page
 
 layout = dbc.Container(     
     [
-        dbc.Row(    #row 1
+        dbc.Row(    #row 1 --> Title
             dbc.Col(
                 [ #col 1
-                html.H1("DATA SCIENCE FOR ALL", className="display-4"),
+                html.H1("DATA SCIENCE FOR ALL COLOMBIA", className="display-3 text-sm-center"),
+                
+                ],width= {"size":12, "order":1},
+            ),
+        ),
+        dbc.Row(    #row 2 --> Subtitle
+            dbc.Col([
                 html.Div(
                     [
-                        html.H4('PROJECT GROUP 162', className="lead"),
-                        html.Div([
-                            dcc.Graph(figure=fig)
-                        ])
+                        html.H1('PROJECT TEAM 162', className="display-4 text-sm-center"),
+                        
 
                     ],
                     ),
                 ],
             ),
-                align="center",
+            align="center",
         ),
+        dbc.Row( #row 3 --> Date picker to select the month
+            dbc.Col(
+                html.Div(
+                    dcc.DatePickerRange(
+                        id='date-picker-range',
+                        min_date_allowed=df.index.to_timestamp().min(),
+                        max_date_allowed=df.index.to_timestamp().max(),
+                        initial_visible_month=df.index.to_timestamp().min(),
+                        start_date=df.index.to_timestamp().min(),
+                        end_date=df.index.to_timestamp().max(),
+                        display_format='MMM YYYY',
+                        className='dark-theme-control',
+                        
+                        
+                    ),
+                    title="Select a date range to see the sales by month",
+                    
+                ), 
+                align="center", 
+                className="text-sm-center mb-4", 
+                width={"size":12, "order":1},
+                
+                
+
+            )  
+        ),
+        dbc.Row(    #row 4 --> Line plot
+        dbc.Col(
+            html.Div(
+                dcc.Graph(figure={}, id='line-plot', style= {'backgroundColor': '#000000', 'color': '#000000'} ),className="mb-4"
+                )
+            )
+        ),
+        dbc.Row([    #row 5 --> second line of graph and cards
+        dbc.Col(
+            html.Div(
+                dcc.Graph(figure={}, id='pie-plot')
+                ),width={"size":6, "order":1},
+            ),
+        dbc.Col([
+            html.Div([
+                dbc.Card([
+                    dbc.CardHeader("Total Sales", className='text-sm-center'),
+                    dbc.CardBody([
+                         html.H2("Sales quantity", className="card-title text-sm-center"),
+                            html.P("This is some card text", className="display-4 text-sm-center",id="card_text1")
+                    ]),
+                ], className="card text-white  mb-3 text-sm-center",id='card1', inverse=True
+                ),
+            ],style={'backgroundColor': '#111111'}),
+            html.Div([
+                dbc.Card([
+                    dbc.CardHeader("Total Sales", className='text-sm-center'),
+                    dbc.CardBody([
+                         html.H2("Amount of money", className=" card-title text-sm-center"),
+                            html.P("Amount of money", className="display-4 text-sm-center",id="card_text2")
+                    ]),
+                ], className="card text-white  mb-3 text-sm-center",id='card2', inverse=True
+            ),
+            ]),
+        ],width={"size":6, "order":1}),
+
+        ]),          
     ],id='home-page',
+    fluid=True,
 )
+
+#############################################################################################################################
+@callback(
+    Output(component_id='line-plot', component_property='figure'),
+    Output(component_id='pie-plot', component_property='figure'),
+    Output(component_id='card_text1', component_property='children'),
+    Output(component_id='card_text2', component_property='children'),
+    [Input(component_id='date-picker-range', component_property='start_date'),
+     Input(component_id='date-picker-range', component_property='end_date')],
+)
+
+def update_line_plot(start_date, end_date):
+    df_filtered = df[(df.index >= start_date) & (df.index <= end_date)]
+    df1_filtered = df1[(df1.index >= start_date) & (df1.index <= end_date)]
+    df2_filtered = df2[(df2.index >= start_date) & (df2.index <= end_date)]
+    ### Figure 1: line-plot
+    fig = px.line(df_filtered, x=df_filtered.index.to_timestamp(), y="count", title='H & M Sales by Month')
+    fig.update_layout(title_text='H & M Sales over the time', title_x=0.5)
+    ### Figure 2: pie-plot
+    fig2 = px.pie(df1_filtered.reset_index(),values='count',names='sales_channel_id', title='Sales by Channel')
+    card1 = '{:,}'.format(df_filtered['count'].sum())
+    card2 = '${:,}'.format(df2_filtered['price'].sum().round(0))
+    
+    return fig, fig2, card1, card2
---
+++ pages/prediction.py
@@ -0,0 +1,206 @@
+import dash
+import pandas as pd
+from dash import html , dcc, callback, Input, Output
+import dash_bootstrap_components as dbc
+from dash_labs.plugins import register_page
+import numpy as np
+import pandas as pd
+import pickle
+
+## Register the page in dash_labs_plugin
+
+register_page(__name__, path="/prediction")
+
+## Define the dataframe and prepare the data 
+
+
+## list of encoders to be used in the prediction function
+
+encoder_garments ={'Under-, Nightwear':1, 'Blouses':2, 'Knitwear':3, 'Special Offers':4,
+    'Shoes':5, 'Accessories':6, 'Skirts':7, 'Trousers':8, 'Dresses Ladies':9,
+    'Jersey Basic':10, 'Jersey Fancy':11, 'Socks and Tights':12, 'Swimwear':13,
+    'Trousers Denim':14, 'Unknown':15, 'Woven/Jersey/Knitted mix Baby':16,
+    'Outdoor':17, 'Shirts':18, 'Dressed':19, 'Dresses/Skirts girls':20, 'Shorts':21}
+    
+encoder_colors ={'Black':1, 'Light Pink':2, 'Light Blue':3, 'Pink':4, 'Dark Green':5,
+    'Dark Grey':6, 'Grey':7, 'Dark Blue':8, 'Dark Pink':9, 'Light Red':10,
+    'Beige':11, 'Dark Yellow':12, 'Green':13, 'Gold':14, 'White':15, 'Light Grey':16,
+    'Red':17, 'Orange':18, 'Light Orange':19, 'Dark Turquoise':20, 'Other':21,
+    'Yellowish Brown':22, 'Dark Red':23, 'Light Beige':24, 'Yellow':25,
+    'Turquoise':26, 'Blue':27, 'Other Pink':28, 'Dark Beige':29, 'Greenish Khaki':30,
+    'Off White':31, 'Light Green':32, 'Silver':33, 'Light Yellow':34,
+    'Light Purple':35, 'Dark Purple':36, 'Purple':37, 'Greyish Beige':38,
+    'Light Turquoise':39, 'Other Blue':40, 'Dark Orange':41, 'Other Turquoise':42,
+    'Bronze/Copper':43, 'Other Red':44, 'Transparent':45, 'Other Yellow':46,
+    'Other Orange':47, 'Other Purple':48, 'Unknown':49, 'Other Green':50}
+    
+encoder_index_groups = {'Ladieswear':1, 'Divided':2, 'Menswear':3, 'Sport':4, 'Baby/Children':5}
+
+
+## Define the layout of the page
+
+layout = dbc.Container([
+    dbc.Row(   #row 1 --> Title
+        dbc.Col([
+            html.Div([
+                html.H1('What channel is gonna be used on this purchase?', className="text-sm-center mb-3"),
+                html.H3("""
+                    Select all the features that you want to use in the prediction.
+                    """, className="text-sm-center"),
+                html.Hr(
+                    style={'color': '#FFFFFF', 'border-color': '#FFFFFF'}
+                )
+            ])
+        ])
+    ),
+    dbc.Row([
+            dbc.Col([
+                html.H4(
+                    'Select the price'
+                ),
+                dcc.Input(
+                    id='price', type='number', placeholder='Price',  min=0, max= 20, step=1, style = {'width': '100%'},
+                    className='mb-3'
+                    
+                ),
+                html.H4(
+                    'Select the month'
+                ),
+                dcc.Dropdown(
+                    id='month', options=[{'label': i, 'value': i} for i in range(1,13)], style = {'color':'#000000'},
+                    className='mb-3'
+                ),
+                html.H4(
+                    'Select the year'
+                ),
+                dcc.Dropdown(
+                    id='year', options=[{'label': i, 'value': i} for i in range(2018,2022)], style = {'color':'#000000'},
+                    className='mb-3'
+                ),
+                html.H4(
+                    'Select the customers age'
+                ),
+                dcc.Input(
+                    id='age', type='number', placeholder='Age',  min=0, max= 100, step=1, style= {'width': '100%'},
+                    className='mb-3'
+                ),
+                html.H4(
+                    'Select the color'
+                ),
+                dcc.Dropdown(
+                    id='color', options=[{'label': i, 'value': i} for i in encoder_colors.keys()], style = {'color':'#000000'},
+                    className='mb-3'
+                ),
+                html.H4(
+                    'Select the category'
+                ),
+                dcc.Dropdown(
+                    id='category', options=[{'label': i, 'value': i} for i in encoder_index_groups.keys()], style = {'color':'#000000'},
+                    className='mb-3'
+                ),
+                html.H4(
+                    'Select the garment'
+                ),
+                dcc.Dropdown(
+                    id='garment', options=[{'label': i, 'value': i} for i in encoder_garments.keys()], style = {'color':'#000000'},
+                    className='mb-3'
+                ),
+                html.H4(
+                    'Select the model to be used'
+                ),
+                dcc.Dropdown(
+                    id='model', options=['Tree decision', 'rf model' ], style = {'color':'#000000'}, className='mb-3'
+                )
+
+            ],width=5),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader("Prediction", className='text-sm-center'),
+                        dbc.CardBody([
+                            html.H2("Digital channel probability", className="card-title text-sm-center"),
+                                html.P("This is some card text", className="display-4 text-sm-center",id="card_text3")
+                    ]),
+                ], className="card text-white  mb-3 text-sm-center",id='card1', inverse=True
+                ),
+                dbc.Card([
+                    dbc.CardHeader("Prediction", className='text-sm-center'),
+                        dbc.CardBody([
+                            html.H2("Store channel probability", className="card-title text-sm-center"),
+                                html.P("This is some card text", className="display-4 text-sm-center",id="card_text4")
+                    ]),
+                ], className="card text-white  mb-3 text-sm-center",id='card1', inverse=True
+                ),
+                dbc.Card([
+                    dbc.CardHeader("Prediction", className='text-sm-center'),
+                        dbc.CardBody([
+                            html.H2("Canal predicho", className="card-title text-sm-center"),
+                                html.P("This is some card text", className="display-4 text-sm-center",id="card_text5")
+                    ]),
+                ], className="card text-white  mb-3 text-sm-center",id='card1', inverse=True
+                ),
+            ], width=7),
+
+    ])
+])
+
+#############################################################################################################################
+@callback(
+    Output('card_text3', 'children'),
+    Output('card_text4', 'children'),
+    Output('card_text5', 'children'),
+    [Input('model', 'value'), 
+    Input('price', 'value'), 
+    Input('month', 'value'), 
+    Input('year', 'value'), 
+    Input('age', 'value'), 
+    Input('color', 'value'), 
+    Input('category', 'value'), 
+    Input('garment', 'value')]
+)
+def prediccion_individual(price = 0.049475,month=5,year = 2018, age = 25,colors = 'Red',group_indexes = 'Ladieswear',garments = 'Dresses Ladies', model = 'tree'):
+    if float(price) <0: 
+        new_price=0 
+    else: new_price = price
+    if month <0: 
+        new_month=5
+    elif  month >12:
+        new_month=5
+    else: new_month = round(month,0)
+    if year <2018: 
+        new_year=2018
+    #elif year > 2020: 
+    #    new_year=2020
+    else: new_year = round(year,0)
+    if age <0: 
+        new_age=25
+    else: new_age = round(age,0)
+    
+    if colors not in list(encoder_colors.keys()):
+        new_color = encoder_colors['Unknown']
+    else: 
+        new_color = encoder_colors[colors]
+        
+    if group_indexes not in list(encoder_index_groups.keys()):
+        new_index = encoder_index_groups['Ladieswear']
+    else: 
+        new_index = encoder_index_groups[group_indexes]
+    
+    if garments not in list(encoder_garments.keys()):
+        new_garment = encoder_garments['Unknown']
+    else: 
+        new_garment = encoder_garments[garments]
+    
+    datos = pd.DataFrame(columns = ['price','month','year','age','colors','group_indexes','garments'], data=np.array([new_price,new_month,new_year,new_age,new_color,new_index,new_garment]).reshape(1,-1))
+    if model == 'tree':
+        loaded_model = pickle.load(open('/home/crnox95/ds4a_project/data/models/tree_model.sav', 'rb'))
+    else:
+        loaded_model = pickle.load(open('ds4a_project/data/models/rf_model.sav', 'rb'))
+    probabilities = loaded_model.predict_proba(datos)
+    predictions = loaded_model.predict(datos)
+    probabilidad_canaldigital = probabilities[0][0]
+    probabilidad_canalpresencial = probabilities[0][1]
+    if predictions == 0:
+        clase_predicha = 'Digital Channel'
+    else:
+        clase_predicha = 'Presential Channel'
+    return probabilidad_canaldigital,probabilidad_canalpresencial, clase_predicha
